98f8309723d3dacc839f9a98559ed2394a435aee
chromasmith <chromasmith1@gmail.com>
2025-11-28 11:52:31 -0800

    Add tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'neon-green': '#00ff41',
+        'spotify-green': '#1DB954',
+        'red-glitch': '#ff0040',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
